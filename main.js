1104f13ea86c3dc9b15dae4e3bf81f275102c85b
Mykerux <michaelverio1412@gmail.com>
2024-02-17 16:44:36 +0700

    Kalkulator Fix

--- main.js
+++ main.js
@@ -1,64 +1,67 @@
 function tampil(num) {
-    let textarea = document.forms.textarea;
-    let jumlah = textarea.value;
+   let textarea = document.forms.textarea;
+   let jumlah = textarea.value;
 
-    let operators = ['+', '-', '*', '%', '.', '/', '00'];
+   let operators = ['+', '-', '*', '%', '.', '/', '00'];
 
-    if (jumlah === '0' && operators.includes(num)) {
-        if (jumlah === '0' && num === '.') {
-            textarea.value = jumlah + num;
-        } else {
-            return;
-        }
-    }
+   if (jumlah === '0' && num === '-') {
+         textarea.value = num;
+      }
 
-    if (jumlah === '0' && num === '0') {
-        if (jumlah.length === 1 && jumlah === '0') {
-            return;
-        } else {
-            textarea.value = jumlah + num;
-        }
-    } else {
-        if (jumlah.length === 1 && jumlah === '0') {
-            textarea.value = num;
-        } else {
-            textarea.value = jumlah + num;
-        }
-    }
+   if (jumlah === '0' && operators.includes(num)) {
+      if (jumlah === '0' && num === '.') {
+         textarea.value = jumlah + num;
+      } else {
+         return;
+      }
+   }
 
-    if (operators.includes(num)) {
-        let lastChar = jumlah.charAt(jumlah.length -1);
+   if (jumlah === '0' && num === '0') {
+      if (jumlah.length === 1 && jumlah === '0') {
+         return;
+      } else {
+         textarea.value = jumlah + num;
+      }
+   } else {
+      if (jumlah.length === 1 && jumlah === '0') {
+         textarea.value = num;
+      } else {
+         textarea.value = jumlah + num;
+      }
+   }
 
-        if (operators.includes(lastChar)) {
-            textarea.value = jumlah.slice(0, -1) + num;
-        } else {
-            textarea.value = jumlah + num;
-        }
-    }
+   if (operators.includes(num)) {
+      let lastChar = jumlah.charAt(jumlah.length -1);
+
+      if (operators.includes(lastChar)) {
+         textarea.value = jumlah.slice(0, -1) + num;
+      } else {
+         textarea.value = jumlah + num;
+      }
+   }
 }
 
 function hasil() {
-    let hasil = document.forms.textarea.value;
+   let hasil = document.forms.textarea.value;
 
-    if (hasil.includes('%')) {
-        let lastChar = hasil.substring(0, hasil.length -1);
+   if (hasil.includes('%')) {
+      let lastChar = hasil.substring(0, hasil.length -1);
+      document.forms.textarea.value = (lastChar / 100);
+   }
 
-        document.forms.textarea.value = eval(lastChar / 100);
-    }
-
-    document.forms.textarea.value = eval(hasil);
+   document.forms.textarea.value = eval(hasil);
 }
 
 function hapusAll() {
-    document.forms.textarea.value = '0';
+   document.forms.textarea.value = '0';
 }
 
 function hapus() {
-    let hasil = document.forms.textarea.value;
+   let hasil = document.forms.textarea.value;
 
-    if (hasil.length === 1) {
-        document.forms.textarea.value = '0';
-    } else {
-        document.forms.textarea.value = hasil.substring(0, hasil.length -1);
-    }
+   if (hasil.length === 1) {
+      document.forms.textarea.value = '0';
+   } else {
+      document.forms.textarea.value = hasil.substring(0, hasil.length -1);
+   }
 }
